a42fbc7c16b0fba8f894a40daef0cdbda5da5f19
mikael04 <mikael.coletto.eng@gmail.com>
2022-06-26 07:45:16 -0300

    Adicionando modelo de regressão e removendo testes;

--- R/AnaliseExploratoria.R
+++ R/AnaliseExploratoria.R
@@ -15,47 +15,32 @@ df_full <- haven::read_sav("data-raw/banco lla e linfoma 16.05.sav")
 df <- df_full |> 
   dplyr::select(PIORMB, dplyr::ends_with(c("MA", "MR", "MD")))
 
-############################################################ #
-## Aprendendo a usar Chi-quadrado
-data("HairEyeColor")
-df_teste <- as.data.frame(HairEyeColor) 
-
-HairEyeColor[,,1]
-
-HairEyeColor
-chisq.test(HairEyeColor[,,1])
-
-chisq.test(df_teste$Sex == 'Male')
-df_teste
-df_teste_male = df_teste[df_teste$Sex == 'Male',]
-table(df_teste_male)
-table()
-
-table(df_teste_male$Hair, df_teste_male$Eye)
-
-library(MASS)        
-print(str(survey))
-
-survey_df <- survey
-# Create a data frame from the main data set.
-stu_data = data.frame(survey$Smoke,survey$Exer)
-
-# Create a contingency table with the needed variables.           
-stu_data = table(survey$Smoke,survey$Exer)
-############################################################ #
-
+### 1.0.1 - Rodando um qui-quadrado para testes ----
 table_ABCC2_RS2273697 <- table(df$PIORMB, df$ABCC2_rs2273697_MA)
 chisq.test(table_ABCC2_RS2273697)
 
-#create data frame with 0 rows and 3 columns
+## 1.1 - Rodando qui-quadrado para todas as variantes ----
+
+#### df_chi2
 df_chi2 <- data.frame(matrix(ncol = 2, nrow = 0))
-#provide column names
 colnames(df_chi2) <- c("Variante", "Chi-2")
 
-for(i in range(2:160)){
+#### Rodando teste para todas as variantes
+for(i in (2:160)){
   testes_chi2 <- chisq.test(table(unlist(df[,1]), unlist(df[,i])), simulate.p.value = TRUE)
   colnames_df[i]
   testes_chi2$p.value
   df_chi2[i-1, ] = c(colnames_df[i], testes_chi2$p.value)
-  print(i)
 }
+
+#### Ordenando por p-value
+df_chi2 <- df_chi2 |> 
+  dplyr::arrange(Variante)
+
+# 2 - Criando modelo de regressão linear múltipla
+
+reg <- lm(data = df, formula = PIORMB ~ .)
+#### Avaliando modelo
+summary(reg)
+
+#### Resultado com multicolinearidade, tratar multicolinearidade
