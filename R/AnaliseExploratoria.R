124f4e887fb602abe392a3b311204fd755cea5c9
mikael04 <mikael.coletto.eng@gmail.com>
2022-07-01 10:09:00 -0300

    Adicionando teste de fisher;

--- R/AnaliseExploratoria.R
+++ R/AnaliseExploratoria.R
@@ -19,29 +19,44 @@ df <- df_full |>
 table_ABCC2_RS2273697 <- table(df$PIORMB, df$ABCC2_rs2273697_MA)
 chisq.test(table_ABCC2_RS2273697)
 
+### 1.0.2 - Rodando um Teste de Fisher para testes ----
+fisher.test(table_ABCC2_RS2273697)
+# fisher.test(table_ABCC2_RS2273697, simulate.p.value = T)
+
 ## 1.1 - Rodando qui-quadrado para todas as variantes ----
 
 #### df_chi2
 df_chi2 <- data.frame(matrix(ncol = 2, nrow = 0))
-colnames(df_chi2) <- c("Variante", "Chi-2")
-colnames_df <- colnames(df)
+colnames(df_chi2) <- c("Variante", "p-value(Chi-2)")
 
+#### df_fisher
+df_fisher <- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(df_fisher) <- c("Variante", "p-value(fisher)")
+
+colnames_df <- colnames(df)
 #### Rodando teste para todas as variantes
 for(i in (2:160)){
+  ## Chi-quadrado
   testes_chi2 <- chisq.test(table(unlist(df[,1]), unlist(df[,i])), simulate.p.value = TRUE)
-  colnames_df[i]
-  testes_chi2$p.value
   df_chi2[i-1, ] = c(colnames_df[i], testes_chi2$p.value)
+  ## Fisher
+  testes_fisher <- fisher.test(table(unlist(df[,1]), unlist(df[,i])), simulate.p.value = TRUE)
+  df_fisher[i-1, ] = c(colnames_df[i], testes_fisher$p.value)
 }
+
+### Investigando erro no for para o teste de fisher
+tabela_teste <- table(unlist(df[,1]), unlist(df[,i]))
+colnames_df[64]
+
 
 #### Ordenando por p-value
 df_chi2 <- df_chi2 |> 
   dplyr::arrange(Variante)
 
-# 2 - Criando modelo de regressão linear múltipla
+# 2 - Criando modelo 
 
-reg <- lm(data = df, formula = PIORMB ~ .)
+
 #### Avaliando modelo
-summary(reg)
+summary()
 
 #### Resultado com multicolinearidade, tratar multicolinearidade
