57f77cb071cf8cc7641a93055bc9c916ce24078f
mikael04 <mikael.coletto.eng@gmail.com>
2022-07-03 16:58:39 -0300

    Adicionando método para criar tabela XLSX do df criado;

--- R/fct_aux/func_tables.R
+++ R/fct_aux/func_tables.R
@@ -40,3 +40,71 @@ fct_table_ad <- function(df_geno_aditivo){
   }
   df_aux
 }
+
+fct_break_gene_variant <- function(df){
+  # df <- df_tabela_aditivo
+  df <- df |> 
+    dplyr::mutate(variant = substr(variant,1,nchar(variant)-3))
+  df <- df |> 
+    tidyr::separate(variant, c("Gene", "Variant"), sep = "_")
+  
+}
+
+fct_merge_cels <- function(df){
+  ## Criar planilha
+  library(openxlsx)
+  wb <- createWorkbook()
+  ## Add a worksheet
+  addWorksheet(wb, "Sheet 1")
+  openxlsx::writeData(wb, "Sheet 1", df, startCol = 1, startRow = 1)
+  df_aux <- df
+  ## Primeiro ordenar df
+  df_aux <- fct_merge_cels_pre_method(df_aux)
+  ## depois fazer o merge de genes
+  fct_merge_cels_gene(df, wb)
+  ## e por último fazer o merge de variantes
+  fct_merge_cels_variant(df, wb)
+  
+  ## Save workbook
+  saveWorkbook(wb, "data-raw/mergeCellsExample.xlsx", overwrite = TRUE)
+}
+
+## Ordenando df para próximos métodos
+fct_merge_cels_pre_method <- function(df){
+  df |> 
+    dplyr::arrange(Gene, Variant, genotype)
+}
+
+fct_merge_cels_gene <- function(df, wb){
+  df_count_gene <- df |> 
+    dplyr::select(Gene) |> 
+    dplyr::group_by(Gene) |> 
+    dplyr::summarise(n = n())
+    dplyr::ungroup()
+  
+  ## Merge cells: gene
+  rows_position_first <- 2
+  for (i in 1:nrow(df_count_gene)){
+    rows_count <- as.numeric(df_count_gene[i, 2])
+    rows_position_last <- rows_position_first - 1 + rows_count
+    mergeCells(wb, "Sheet 1", cols = 1, rows = (rows_position_first):rows_position_last)
+    rows_position_first <- rows_position_last + 1
+  }
+}
+fct_merge_cels_variant <- function(df, wb){
+  df_count_variant <- df |> 
+    dplyr::select(Variant) |> 
+    dplyr::group_by(Variant) |> 
+    dplyr::mutate(n = dplyr::n()) |> 
+    dplyr::distinct(Variant, .keep_all = T) |> 
+    dplyr::ungroup()
+  
+  ## Merge cells: gene
+  rows_position_first <- 2
+  for (i in 1:nrow(df_count_variant)){
+    rows_count <- as.numeric(df_count_variant[i, 2])
+    rows_position_last <- rows_position_first - 1 + rows_count
+    mergeCells(wb, "Sheet 1", cols = 2, rows = (rows_position_first):rows_position_last)
+    rows_position_first <- rows_position_last + 1
+  }
+}
