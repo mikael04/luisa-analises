5ff61fbac568c839a65de7203f8f9797ba08da51
mikael04 <mikael.coletto.eng@gmail.com>
2022-07-03 10:58:24 -0300

    Commit durante a criação da tabela chi-2;

--- R/AnaliseExploratoria.R
+++ R/AnaliseExploratoria.R
@@ -10,10 +10,11 @@ library(dplyr)
 
 
 # 1 - Lendo Base de dados ----
-df_full <- haven::read_sav("data-raw/banco lla e linfoma 16.05.sav")
+df_full <- haven::read_sav("data-raw/banco lla e linfoma 16.05.sav") |> 
+  dplyr::filter(!is.na(PIORMB))
 
 df <- df_full |> 
-  dplyr::select(PIORMB, dplyr::ends_with(c("MA", "MR", "MD")))
+  dplyr::select(PIORMB, dplyr::ends_with(c("MA", "MR", "MD", "MU")))
 
 ### 1.0.1 - Rodando um qui-quadrado para testes ----
 table_ABCC2_RS2273697 <- table(df$PIORMB, df$ABCC2_rs2273697_MA)
@@ -54,11 +55,50 @@ df_chi2 <- df_chi2 |>
   dplyr::arrange(Variante)
 
 ## 1.2 Criando tabela Chi-2
-table_ABCC2_RS2273697
-colnames_df[2]
 
-df_aux <- df_full |> 
-  dplyr::select(PIORMB, ABCC2_rs2273697_MO, ABCC2_rs2273697_MA, ABCC2_rs2273697_MR, ABCC2_rs2273697_MD)
+df_modelos_genotipos <- data.frame(df_full[, 32], df_full[, 57:324])
+
+# df_rs17222723 <- data.frame(df_modelos_genotipos[, 1], df_modelos_genotipos[, 8:9])
+
+df_aux_geno_aditivo <- df_modelos_genotipos |> 
+  dplyr::select(!ends_with(c("MR", "MD", "MU")))
+
+colnames_df_ad <- colnames(df_aux_geno_aditivo)
+colnames_just_ad <- c("PIORMB")
+for(i in 2:(length(colnames_df_ad)-1)){
+  this_str <- substr(colnames_df_ad[i], 1, nchar(colnames_df_ad[i])-3)
+  next_str <- substr(colnames_df_ad[i+1], 1, nchar(colnames_df_ad[i+1])-3)
+  previous_str <- substr(colnames_df_ad[i-1], 1, nchar(colnames_df_ad[i-1])-3)
+  if(this_str == previous_str){
+    ## Mantém a variante
+    colnames_just_ad <- c(colnames_just_ad, colnames_df_ad[i], colnames_df_ad[i-1])
+  }else{
+    ## Não adiciona a variante, pois não tem modelo aditivo
+  }
+  if(this_str == next_str){
+    ## Mantém a variante
+    colnames_just_ad <- c(colnames_just_ad, colnames_df_ad[i], colnames_df_ad[i+1])
+  }else{
+    ## Não adiciona a variante, pois não tem modelo aditivo
+  }
+}
+
+colnames_just_ad <- unique(unlist(strsplit(colnames_just_ad, " ")))
+
+df_geno_aditivo <- df_aux_geno_aditivo |> 
+  dplyr::select(any_of(colnames_just_ad))
+
+df_geno_aditivo_test <- df_geno_aditivo[,1:3] |> 
+  dplyr::mutate(PIORMB = ifelse(PIORMB > 0, 1, 0)) |> 
+  dplyr::group_by(ABCC2_rs2273697_MO, PIORMB) |> 
+  dplyr::mutate(count = n()) |> 
+  dplyr::distinct(ABCC2_rs2273697_MO, PIORMB, .keep_all = T) |>
+  dplyr::select(-ABCC2_rs2273697_MA) |> 
+  dplyr::ungroup()
+
+
+# df_aux <- df_full |> 
+#   dplyr::select(PIORMB, ABCC2_rs2273697_MO, ABCC2_rs2273697_MA, ABCC2_rs2273697_MR, ABCC2_rs2273697_MD)
 
 ## Lendo outra  tabela auxiliar
 
