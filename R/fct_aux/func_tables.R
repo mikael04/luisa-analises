fa788a2b78cf82c0dbd16f0435480e67d67ec748
mikael04 <mikael.coletto.eng@gmail.com>
2022-07-04 08:59:09 -0300

    Removendo partes do merge que foram para outro arquivo; Adicionando funções para criar df com nomes de genes(necessário para modelos recessivos, dominantes e únicos); Adicionando método para criar tabela geral (recessivos, dominantes e unicos);

--- R/fct_aux/func_tables.R
+++ R/fct_aux/func_tables.R
@@ -1,7 +1,7 @@
 ##################################################################### #
 ##' Função para criar tabela de modelos aditivos
 ##' 
-##' Autores: Mikael
+##' Autores: Mikael e Matheus
 ##' Data: 03/07/2022
 ##################################################################### #
 
@@ -38,30 +38,9 @@ fct_table_ad <- function(df_geno_aditivo){
     
     df_aux <- rbind(df_aux, df_)
   }
-  df_aux
+  fct_break_gene_variant(df_aux)
 }
 
-fct_merge_cels <- function(df_merge){
-  # df_merge <- df_tabela_aditivo
-  ## Criar planilha
-  library(openxlsx)
-  wb <- createWorkbook()
-  ## Add a worksheet
-  addWorksheet(wb, "Sheet 1")
-  ## Quebrando coluna Gene e variante
-  df_aux <- fct_break_gene_variant(df_merge)
-  ## Primeiro ordenar df
-  df_aux <- fct_merge_cels_pre_method(df_aux)
-  ## Criando workbook
-  openxlsx::writeData(wb, "Sheet 1", df_aux, startCol = 1, startRow = 1)
-  ## depois fazer o merge de genes
-  fct_merge_cels_gene(df_aux, wb)
-  ## e por último fazer o merge de variantes
-  fct_merge_cels_variant(df_aux, wb)
-  
-  ## Save workbook
-  saveWorkbook(wb, "data-raw/mergeCellsExample.xlsx", overwrite = TRUE)
-}
 
 fct_break_gene_variant <- function(df_break){
   # df_break <- df_tabela_aditivo
@@ -72,44 +51,62 @@ fct_break_gene_variant <- function(df_break){
   
 }
 
-## Ordenando df para próximos métodos
-fct_merge_cels_pre_method <- function(df_pre){
-  df_pre |> 
-    dplyr::arrange(Gene, Variant, genotype)
+## Função que retornará uma tabela com os nomes dos genótipos agrupados pelo valor do modelo recessivo
+fct_table_rec_get_names <- function(df_geno_others_sel, cols_names){
+  # cols_names <- cols_sel
+  # df_geno_others_sel <- df_recessivo_names
+  df_recessivo_test <- df_geno_others_sel |>
+    dplyr::select(all_of(cols_names)) |> ## Cria o df de teste para um modelo
+    dplyr::group_by(across(cols_names)) |> ## Determina o agrupamento
+    dplyr::summarise() |> 
+    dplyr::ungroup() |> 
+    dplyr::group_by(across(cols_names[2])) |> ## Define outro agrupamento
+    dplyr::mutate(new_geno = paste((!!as.symbol(cols_names[1])), collapse = " or ")) |>
+    dplyr::select(all_of(c(cols_names[2], "new_geno"))) |> ## remove a primeira coluna, mantendo apenas a coluna com valores agrupados
+    dplyr::distinct(new_geno) |> 
+    dplyr::ungroup()
+  
+  colnames(df_recessivo_test) <- c(cols_names[2], cols_names[1])
+  
+  df_recessivo_test
 }
 
-fct_merge_cels_gene <- function(df_merge_gene, wb){
-  # df_merge_gene <- df_aux
-  df_count_gene <- df_merge_gene |> 
-    dplyr::select(Gene) |> 
-    dplyr::group_by(Gene) |> 
-    dplyr::summarise(n = n()) |> 
-    dplyr::ungroup()
-  
-  ## Merge cells: gene
-  rows_position_first <- 2
-  for (i in 1:nrow(df_count_gene)){
-    rows_count <- as.numeric(df_count_gene[i, 2])
-    rows_position_last <- rows_position_first - 1 + rows_count
-    mergeCells(wb, "Sheet 1", cols = 1, rows = (rows_position_first):rows_position_last)
-    rows_position_first <- rows_position_last + 1
+## Função que recebe tabela com colunas genótipo (MO) e modelo recessivo (MR)
+fct_table_rec_dom_un <- function(df_geno_others){
+  df_aux <- data.frame(matrix(nrow = 0, ncol = 4))
+  colnames(df_aux) <- c("variant", "genotype", "0", "1")
+  for(i in seq(3, ncol(df_geno_others), 2)){
+    df_this_col <- data.frame(df_geno_others[,1], df_geno_others[,i])
+    cols_df <- c("PIORMB", colnames(df_geno_others[i]))
+    colnames(df_this_col) <- cols_df
+    
+    df_recessivo_names <- data.frame(df_geno_others[,i-1], df_geno_others[,i])
+    cols_sel <- c(colnames(df_geno_others[i-1]), colnames(df_geno_others[i]))
+    colnames(df_recessivo_names) <- cols_sel
+    
+    df_recessivo_names_aux <- fct_table_rec_get_names(df_recessivo_names, cols_sel)
+    
+    df_geno_others_test <- df_this_col |> 
+      dplyr::mutate(PIORMB = ifelse(PIORMB > 0, 1, 0)) |> 
+      dplyr::group_by_at(cols_df) |> 
+      dplyr::summarise(count = n()) |> 
+      dplyr::ungroup()
+    
+    cols_arrange <- c(colnames(df_geno_others[i]), "PIORMB")
+    df_geno_others_test <- df_geno_others_test |> 
+      dplyr::arrange(across(all_of(cols_arrange)))
+    
+    df_ <- df_geno_others_test |> 
+      tidyr::pivot_wider(names_from = PIORMB, values_from = count)
+    
+    df_$variant <- cols_df[2]
+    df_ <- dplyr::inner_join(df_, df_recessivo_names_aux, by = cols_df[2])
+    
+    colnames(df_) <- c("old", "0", "1", "variant", "genotype")
+    df_ <- df_ |> 
+      dplyr::select("variant", "genotype", "0", "1")
+    
+    df_aux <- rbind(df_aux, df_)
   }
-}
-fct_merge_cels_variant <- function(df_merge_variant, wb){
-  # df_merge_variant <- df_aux
-  df_count_variant <- df_merge_variant |> 
-    dplyr::select(Variant) |> 
-    dplyr::group_by(Variant) |> 
-    dplyr::mutate(n = dplyr::n()) |> 
-    dplyr::distinct(Variant, .keep_all = T) |> 
-    dplyr::ungroup()
-  
-  ## Merge cells: gene
-  rows_position_first <- 2
-  for (i in 1:nrow(df_count_variant)){
-    rows_count <- as.numeric(df_count_variant[i, 2])
-    rows_position_last <- rows_position_first - 1 + rows_count
-    mergeCells(wb, "Sheet 1", cols = 2, rows = (rows_position_first):rows_position_last)
-    rows_position_first <- rows_position_last + 1
-  }
+  fct_break_gene_variant(df_aux)
 }
