417d6b43d4cc701abef5450c759bd174ee6de231
mikael04 <mikael.coletto.eng@gmail.com>
2022-07-06 11:48:24 -0300

    Alterando parcialmente a função para adicionar novo método que fará os testes de chi-2 e fisher; Removendo o modelo único; Adicionando parâmetro para agrupamento;

--- R/AnaliseExploratoria.R
+++ R/AnaliseExploratoria.R
@@ -7,59 +7,46 @@
 
 # 0 - Scripts e bibliotecas ----
 library(dplyr)
+source("R/fct_aux/func_calc_perc.R")
 source("R/fct_aux/func_remove_columns.R")
 source("R/fct_aux/func_tables.R")
+source("R/fct_aux/func_testes_chi2_fisher.R")
 source("R/fct_aux/func_writ_organ_xlsx.R")
-source("R/fct_aux/func_calc_perc.R")
 
 
 # 1 - Lendo Base de dados ----
 df_full <- haven::read_sav("data-raw/banco lla e linfoma 16.05.sav") |> 
   dplyr::filter(!is.na(PIORMB))
 
-df <- df_full |> 
-  dplyr::select(PIORMB, dplyr::ends_with(c("MA", "MR", "MD", "MU")))
-
 set.seed(42)
 
-## 1.1 - Rodando qui-quadrado para todas as variantes ----
+# 2 - Rodando testes ----
 
-#### df_chi2
-df_chi2 <- data.frame(matrix(ncol = 2, nrow = 0))
-colnames(df_chi2) <- c("variant", "p-value(Chi-2)")
+## 2.1 - Teste 1 (Presença) ----
+## Primeiro será rodado para agrupamento não possui VS possui
+## Ajustaremos a base de dados para o novo agrupamento (utilizando a variavel "PIORMB")
 
-#### df_fisher
-df_fisher <- data.frame(matrix(ncol = 2, nrow = 0))
-colnames(df_fisher) <- c("variant", "p-value(fisher)")
+df_abs_or_pres <- df_full |> 
+  dplyr::select(PIORMB, dplyr::ends_with(c("MA", "MR", "MD"))) |> 
+  dplyr::mutate(PIORMB = ifelse(PIORMB > 0, 1, 0))
 
-colnames_df <- colnames(df)
-## Rodando teste para todas as variantes
-for(i in (2:ncol(df))){
-  ## Chi-quadrado
-  testes_chi2 <- chisq.test(table(unlist(df[,1]), unlist(df[,i])), simulate.p.value = TRUE)
-  df_chi2[i-1, ] = c(colnames_df[i], testes_chi2$p.value)
-  ## Fisher
-  tabela_fisher <- table(unlist(df[,1]), unlist(df[,i]))
-  if(ncol(tabela_fisher) > 1){
-    testes_fisher <- fisher.test(tabela_fisher, simulate.p.value = TRUE)
-    df_fisher[i-1, ] = c(colnames_df[i], testes_fisher$p.value)
-  }else{
-    df_fisher[i-1, ] = c(colnames_df[i], -1)
-  }
+## Rodando teste
+list_testes <- fct_testes_chi2_fisher(df_abs_or_pres)
+
+if(list_testes[[1]]){
+  ## Testes sem simulação de Monte Carlo
+  df_chi2 <- list_testes[[2]]
+  df_fisher <- list_testes[[3]]
 }
-## Ordenando por p-value
-df_chi2 <- df_chi2 |> 
-  dplyr::arrange(variant)
 
-## 1.2 - Criando tabela Chi-2 ----
+# 3 - Avaliando modelos (dominante, recessivo, aditivo)
 
 df_modelos_genotipos <- data.frame(df_full[, 32], df_full[, 57:324])
-
-#### 1.2.1 - Tabela para modelos aditivos ----
+### 3.1 - Tabela para modelos aditivos ----
 df_geno_aditivo <- df_modelos_genotipos |> 
   dplyr::select(dplyr::ends_with(c("PIORMB", "MO")))
 
-df_tabela_aditivo <- fct_table_ad(df_geno_aditivo)
+df_tabela_aditivo <- fct_table_ad(df_geno_aditivo, "pres")
 
 #### Adicionar p-value a tabela
 df_chi2_adit <- fct_break_gene_variant_ends(df_chi2, "_MA")
@@ -78,7 +65,7 @@ fct_merge_cels(df_tabela_aditivo_p_perc, excel_name)
 ### 1.2.2 - Tabela para modelo recessivo ----
 df_geno_recessivo <- fct_remove_columns(df_modelos_genotipos, c("MA", "MD", "MU"))
 
-df_tabela_recessivo <- fct_table_rec_dom_un(df_geno_recessivo)
+df_tabela_recessivo <- fct_table_rec_dom_un(df_geno_recessivo, "pres")
 
 #### Adicionar p-value a tabela
 df_chi2_rec <- fct_break_gene_variant_ends(df_chi2, "_MR")
@@ -97,7 +84,7 @@ fct_merge_cels(df_tabela_recessivo_p_perc, excel_name)
 ### 1.2.3 - Tabela para modelo dominante ----
 df_geno_dominante <- fct_remove_columns(df_modelos_genotipos, c("MA", "MR", "MU"))
 
-df_tabela_dominante <- fct_table_rec_dom_un(df_geno_dominante)
+df_tabela_dominante <- fct_table_rec_dom_un(df_geno_dominante, "pres")
 
 #### Adicionar p-value a tabela
 df_chi2_dom <- fct_break_gene_variant_ends(df_chi2, "_MR")
@@ -112,29 +99,6 @@ df_tabela_dominante_p_perc <- fct_calc_perc(df_tabela_dominante_p)
 
 excel_name <- "tabela_modelo_dominante"
 fct_merge_cels(df_tabela_dominante_p_perc, excel_name)
-
-
-### 1.2.4 - Tabela para modelo unico ----
-df_geno_unico <- fct_remove_columns(df_modelos_genotipos, c("MA", "MD", "MR"))
-
-df_tabela_unico <- fct_table_rec_dom_un(df_geno_unico)
-
-#### Adicionar p-value a tabela
-df_chi2_un <- fct_break_gene_variant_ends(df_chi2, "_MU")
-
-df_chi2_un$`p-value(Chi-2)` <- round(as.numeric(df_chi2_un$`p-value(Chi-2)`), 4)
-df_tabela_unico_p <- dplyr::inner_join(df_tabela_unico, df_chi2_un, by = c("Gene", "variant"))
-
-## Calculando percentual da coluna de frequencia
-df_tabela_unico_p_perc <- fct_calc_perc(df_tabela_unico_p)
-
-#### Criar tabela XLSX do df criado (modelo unico)
-
-excel_name <- "tabela_modelo_unico"
-fct_merge_cels(df_tabela_unico_p_perc, excel_name)
-
-
-# 2 - Criando modelo 
 
 
 #### Avaliando modelo
