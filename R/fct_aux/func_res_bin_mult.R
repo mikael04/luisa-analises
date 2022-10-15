3558669ed722ff1fccc9ff481051f6430f777a88
mikael04 <mikael.coletto.eng@gmail.com>
2022-10-15 07:42:49 -0300

    Adicionando mais alguns passos, ainda não finalizado;

--- R/fct_aux/func_res_bin_mult.R
+++ R/fct_aux/func_res_bin_mult.R
@@ -34,8 +34,13 @@ fct_vars_pvalue <- function(model, debug){
   cbind(vars_aux, p_value)
 }
 
+## 1.2 -Cria dataframe usado para o join (genótipo e dummie)  ----
+## Recebe o dataframe separado apenas com variantes selecionadas
 
-# 1 - Função ----
+## 1.3 - Resultado da binomial múltipla ----
+## Vai receber o df do protocolo selecionado, o modelo e o nome do modelo
+## Fará primeiro o join da tabela já filtrada com os genótipos e suas dummies
+## Para ter o gene base + a variação, e pode escrever isso na tabela de resultados
 fct_res_bin_mult <- function(df, model, model_name, switch_write_table){
   if(teste_interno){
     model <- ctx_abs_or_pres_binom_mult
@@ -52,8 +57,16 @@ fct_res_bin_mult <- function(df, model, model_name, switch_write_table){
   vars_model_genotype <- gsub(pattern = '.{1}$', "O", vars_model)
   vars_selected <- stringr::str_sort(c(vars_model, vars_model_genotype))
   
-  df <- df |> 
-    dplyr::select(all_of(vars_selected))
+  ## Criando dfs (um pouco simplificado) com variantes (genótipos e modelos usados)
+  df_all_vars <- df |> 
+    dplyr::select(all_of(vars_selected)) |> 
+    dplyr::distinct(across(all_of(vars_selected)))
+  
+  ## Separando dfs por variante
+  num_vars <- length(unique(vars_model))
+  
+  df_teste <- as.data.frame(t(df_all_vars)) |> 
+    tibble::rownames_to_column("gene_variant_model")
   
   ## Criando nome de protocolo e agrupamento para nome do arquivo excel
   model_name <- gsub("_binom_mult", "", model_name)
