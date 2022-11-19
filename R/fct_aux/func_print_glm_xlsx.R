4ce212db713ed94bd699a24824452a1a897a4d0c
mikael04 <mikael.coletto.eng@gmail.com>
2022-11-19 06:20:24 -0300

    Adicionando odds ratio e intervalo de confiança;

--- R/fct_aux/func_print_glm_xlsx.R
+++ R/fct_aux/func_print_glm_xlsx.R
@@ -11,11 +11,18 @@
 fct_print_glm_xlsx <- function(model, switch_write_table){
   model_name <- deparse(substitute(model))
   vars <- names(model$qr$qr[2,])
+  vars <- gsub(".{1}$", "", vars)
+  vars[1] <- "(Intercept)"
   estimate <- round(coef(summary(model))[,"Estimate"], 4)
   z_value <- round(coef(summary(model))[,"z value"], 4)
+  odd.ratio <- round(exp(estimate), 2)
   p_value <- round(coef(summary(model))[,"Pr(>|z|)"], 4)
+  df_confint <- as.data.frame(exp(confint(model))) |> 
+    dplyr::mutate(`2.5 %` = round(`2.5 %`, 4),
+                  `97.5 %` = round(`97.5 %`, 4))
   
-  tabela <- data.frame(vars, estimate, z_value, p_value)
+  tabela <- data.frame(vars, estimate, odd.ratio, df_confint, z_value, p_value) |> 
+    dplyr::rename(`2.5%` = `X2.5..`, `97.5%` = `X97.5..`)
   
   if(switch_write_table){
     writexl::write_xlsx(tabela, paste0("data-raw/tabela-reg-bin/resultado_", model_name, ".xlsx"))
